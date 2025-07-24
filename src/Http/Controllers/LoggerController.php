971f9db44786d593fff3798de2186affa262e131
carlosedu011brasil <carlos.eduardo@011brasil.com.br>
2025-07-24 10:25:44 -0300

    feat: adiciona LoggerController para expor logger.json via rota

---
+++ src/Http/Controllers/LoggerController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Vendor\Pacote\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+class LoggerController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        $jsonPath = storage_path('app/public/logger.json');
+
+        if (!file_exists($jsonPath)) {
+            return response()->json(['error' => 'Arquivo não encontrado'], 404);
+        }
+
+        $content = file_get_contents($jsonPath);
+        return response($content, 200)->header('Content-Type', 'application/json');
+    }
+}
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,6 @@
 <?php
-use Illuminate\Support\Facades\Route;
 
-Route::get('/logger-data', function () {
-    $json = storage_path('app/public/logger.json');
-    return file_exists($json) ? response()->file($json) : response()->json(['error' => 'Arquivo não encontrado'], 404);
-});
+use Illuminate\Support\Facades\Route;
+use Vendor\Pacote\Http\Controllers\LoggerController;
+
+Route::get('/logger-data', LoggerController::class);
