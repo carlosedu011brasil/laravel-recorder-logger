f0dc70b0158245bee80423ffb993d7347dfd99d6
carlosedu011brasil <carlos.eduardo@011brasil.com.br>
2025-07-24 09:43:37 -0300

    Create logs route to view

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/recorder/log', function () {
+    $path = storage_path('app/recorder-log.json');
+
+    if (!file_exists($path)) {
+        return response()->json(['message' => 'Nenhum log disponível'], 404);
+    }
+
+    $data = json_decode(file_get_contents($path), true);
+    return response()->json($data);
+})->name('getRecorderLog');
--- src/RecorderLoggerServiceProvider.php
+++ src/RecorderLoggerServiceProvider.php
@@ -8,8 +8,14 @@ class RecorderLoggerServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+
+        // Carregar as rotas do pacote
+        $this->loadRoutesForm(__DIR__.'/../routes/web.php')
+
+        // publicar componente
         $this->publishes([
             __DIR__.'/../resources/js/Components' => resource_path('js/Components/RecorderLogger'),
         ], 'recorder-components');
+
     }
 }
